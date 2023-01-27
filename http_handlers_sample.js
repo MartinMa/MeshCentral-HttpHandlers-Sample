2f633b08d0d5de2b9414f45807ef7cef05acf36f
Martin Mädler <martin.maedler@gmail.com>
2023-01-27 13:01:05 +0100

    Update http_handlers_sample.js

--- http_handlers_sample.js
+++ http_handlers_sample.js
@@ -8,21 +8,28 @@
 
 "use strict";
 
-module.exports.http_handlers_sample = function (obj, parent) {
-    for (var i in parent.config.domains) {
-        // This is a subdomain with a DNS name, no added HTTP bindings needed.
-        if ((parent.config.domains[i].dns != null) || (parent.config.domains[i].share != null)) { continue; }
-        var domain = parent.config.domains[i];
-        var url = domain.url;
-        // Add custom endpoints here.
-        obj.app.get(url + 'api/custom', function (req, res) {
-            // Optionally check for active user session here.
-            // Example of a simple REST API returning a JSON object.
-            res.set({ 'Content-Type': 'application/json' });
-            res.status(200);
-            res.send(JSON.stringify({
-                value: 'test'
-            }));
-        });
+module.exports.http_handlers_sample = function (parent) {
+    var obj = {};
+    obj.parent = parent;
+
+    obj.hook_setupHttpHandlers = function(obj, parent) {
+        for (var i in parent.config.domains) {
+            // This is a subdomain with a DNS name, no added HTTP bindings needed.
+            if ((parent.config.domains[i].dns != null) || (parent.config.domains[i].share != null)) { continue; }
+            var domain = parent.config.domains[i];
+            var url = domain.url;
+            // Add custom endpoints here.
+            obj.app.get(url + 'api/custom', function (req, res) {
+                // Optionally check for active user session here.
+                // Example of a simple REST API returning a JSON object.
+                res.set({ 'Content-Type': 'application/json' });
+                res.status(200);
+                res.send(JSON.stringify({
+                    value: 'test'
+                }));
+            });
+        }
     }
+
+    return obj;
 }
